c50fc38f070b36262ed747231fff8667ed2c40be
Helena Saborit <helena.sb91@gmail.com>
2023-12-21 17:08:29 +0100

    changed cosplay model and routes according new DB

--- models/Cosplay.model.js
+++ models/Cosplay.model.js
@@ -1,32 +1,21 @@
 const { Schema, model } = require("mongoose");
-// const family = require("../utils/family");
 
 const CosplaySchema = new Schema({
-    name: {
-        type: String,
-        required: true,  
-    }, 
-    image: {
-        type: String,
-        //! here to be fed by the json file.  HOW. 
-        default: "https://www.shutterstock.com/image-vector/user-profile-icon-vector-avatar-600nw-2247726673.jpg"
-    },
-    family: [
-        {
-            //! check how to filter this ???
-            type: Schema.Types.ObjectId,
-            enum: "Family",
-        },
-    ],
-    description: String,
+    name: String, 
+    nameDetails: String,
+    image: String,
+    cosplayImage: String,
+    serie: String,
+    serieDetails: String,
+    descriptionCat: String,
+    descriptionEsp: String,
     choosedBy: {
         //feeds from User.model
         type: Schema.Types.ObjectId,
         ref: "User",
-        default: null,
     },
-    link1: String, //youtube
-    link2: String, //aliexpress
+    youtubeLink: String, 
+    aliExpressLink: String, 
 });
 
 const Cosplay = model("Cosplay", CosplaySchema);
--- routes/cosplay.routes.js
+++ routes/cosplay.routes.js
@@ -1,51 +1,14 @@
 const Cosplay = require("../models/Cosplay.model");
 const router = require("express").Router();
-const family = require("../utils/family");
-const isLogged = require("../middlewares/auth");
-
-//! cosplay-family --> to show by grouping ?
-
 
 //GET "api/cosplay/cosplay-list" --> shows a list of all cosplays
 router.get("/cosplay-list", async (req, res, next) => {
     try {
         const response = await Cosplay.find() //.select("name")
         res.status(200).json(response)
-        console.log("postman accediendo a la ruta");
-
+        
     } catch(err){
         next(err)
-    }
-    
-});
-
-//GET "/api/cosplay/family" --> show family in Cosplay
-router.get("/family", async (req, res, next) =>  {
-    res.status(200).json(family);
-});
-
-//POST "api/cosplay/create-cosplay" --> create new cosplay (receives details from new cosplay in FE and creates new cosplay in DB)
-router.post("/create-cosplay", async (req, res, next) => {
-    const {name, image, family, description, choosedBy, link1, link2 } = req.body;
-
-    //get data from FE to send BE
-    const newCosplay = {
-        name: name,
-        image,
-        // family,
-        description: description,
-        // choosedBy: _id,
-        link1,
-        link2,
-    };
-    console.log("newCosplay", newCosplay);
-
-    try {
-        await Cosplay.create(newCosplay);
-        res.status(201).json("New cosplay created in DB");
-    } catch (err){
-        console.log("an error when creating cosplay occurred");
-        next(err);
     }
 });
 
@@ -55,7 +18,8 @@ router.get("/:cosplayId/details", async (req, res, next) => {
     const { cosplayId } = req.params;
   
     try {
-      const response = await Cosplay.findById(cosplayId).populate("choosedBy");
+      const response = await Cosplay.findById(cosplayId)//.populate("choosedBy");
+    //   console.log("cosplay details route: ", response)
       res.status(200).json(response);
     } catch (error) {
       next(error);
@@ -80,5 +44,29 @@ router.patch("/:cosplayId/choose-cosplay", async (req, res, next) => {
 
 //! FALTA UN-CHOOSE COSPLAY ROUTE
 
+//POST "api/cosplay/create-cosplay" --> create new cosplay (receives details from new cosplay in FE and creates new cosplay in DB)
+// router.post("/create-cosplay", async (req, res, next) => {
+//     const {name, image, description, choosedBy, link1, link2 } = req.body;
+
+//     //get data from FE to send BE
+//     const newCosplay = {
+//         name: name,
+//         image,
+
+//         description: description,
+//         // choosedBy: _id,
+//         link1,
+//         link2,
+//     };
+//     console.log("newCosplay", newCosplay);
+
+//     try {
+//         await Cosplay.create(newCosplay);
+//         res.status(201).json("New cosplay created in DB");
+//     } catch (err){
+//         console.log("an error when creating cosplay occurred");
+//         next(err);
+//     }
+// });
 
 module.exports = router;
