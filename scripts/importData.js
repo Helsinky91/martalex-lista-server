d8f215a00295d78b6a0f9416af272fab8eafb02e
Helena Saborit <helena.sb91@gmail.com>
2023-12-22 15:23:15 +0100

    connected JSON to DB

--- scripts/importData.js
+++ scripts/importData.js
@@ -1,26 +1,30 @@
-// const fs = require('fs');
-// const { MongoClient } = require('mongodb');
+const fs = require('fs');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
-// const uri = 'your-mongodb-atlas-uri';
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// async function run() {
-//   try {
-//     await client.connect();
-//     const database = client.db('your-database-name');
-//     const collection = database.collection('your-collection-name');
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db('martalex-cosplaylist'); 
+    const collection = database.collection('cosplays'); 
+    // console.log('JSON Data:', collection);
 
-//     // Read JSON file
-//     const jsonData = fs.readFileSync('path/to/your/file.json');
-//     const documents = JSON.parse(jsonData);
+    // Read JSON file
+    const jsonData = fs.readFileSync('./scripts/martalexCosplays.json'); 
+    const documents = JSON.parse(jsonData); 
 
-//     // Insert documents into the collection
-//     await collection.insertMany(documents);
+    // Insert documents into the collection
+    await collection.insertMany(documents);
 
-//     console.log('Data inserted successfully');
-//   } finally {
-//     await client.close();
-//   }
-// }
+    console.log('Data inserted successfully');
+} catch (error) {
+  console.error('Error:', error);
+} finally {
+  await client.close();
+}
+}
 
-// run();
+run();
