b4e0fdc089a5185f7d2ebb964ce9387202da72d0
Helena Saborit <helena.sb91@gmail.com>
2024-06-04 15:39:13 +0200

    reverted CORS

--- app.js
+++ app.js
@@ -11,22 +11,22 @@ const express = require("express");
 const cors = require('cors');
 const app = express();
 
-// ℹ️ Enable CORS for all routes
-// app.use(cors());
-app.use(
-    cors({
-      origin: "martalex-server.netlify.app", // Explicitly specify the allowed origin
-      credentials: true, // Important for cookies, authorization headers with HTTPS
-      methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-      allowedHeaders: [
-        "Origin",
-        "Content-Type",
-        "Accept",
-        "Authorization",
-        "X-Request-With",
-      ],
-    })
-  );
+//ℹ️ Enable CORS for all routes
+app.use(cors());
+// app.use(
+//     cors({
+//       origin: "martalex-server.netlify.app", // Explicitly specify the allowed origin
+//       credentials: true, // Important for cookies, authorization headers with HTTPS
+//       methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+//       allowedHeaders: [
+//         "Origin",
+//         "Content-Type",
+//         "Accept",
+//         "Authorization",
+//         "X-Request-With",
+//       ],
+//     })
+//   );
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
